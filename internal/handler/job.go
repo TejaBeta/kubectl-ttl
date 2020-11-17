671b3c99ee18f22a258e2a7f4501a4881ebe83bf
TejaBeta <tejasvi.thota@gmail.com>
2020-11-17 08:44:58 +0000

    Addition of create job is complete

--- internal/handler/job.go
+++ internal/handler/job.go
@@ -14,7 +14,61 @@ limitations under the License.
 
 package handler
 
-// CreateJob function is used to create a job to execute ttl
-func CreateJob(ns string, kind string, name string, time int) {
+import (
+	"fmt"
 
+	log "github.com/sirupsen/logrus"
+	"github.com/tejabeta/kubectl-ttl/pkg/k8s"
+	batchv1 "k8s.io/api/batch/v1"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// CreateJob function is used to create a job to execute ttl
+func CreateJob(ns string, kind string, name string, sa string, time int) {
+	var ttlSecondsAfterFinished, backOffLimit int32 = 300, 3
+
+	jobContainer := v1.Container{
+		Name:    "ttl-" + kind + "-Container",
+		Image:   "bitnami/kubectl",
+		Command: []string{"kubectl", "delete", kind + "/" + name, "-n", ns},
+	}
+
+	initContainer := v1.Container{
+		Name:    "ttl-" + kind + "-Init-Container",
+		Image:   "busybox:1.28",
+		Command: []string{"sh", "-c", "sleep " + fmt.Sprint(time) + "m"},
+	}
+
+	job := &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{Name: "ttl-" + kind + "-job", Namespace: ns},
+		Spec: batchv1.JobSpec{
+			TTLSecondsAfterFinished: &ttlSecondsAfterFinished,
+			BackoffLimit:            &backOffLimit,
+			Template: v1.PodTemplateSpec{
+				Spec: v1.PodSpec{
+					ServiceAccountName: sa,
+					Containers:         []v1.Container{jobContainer},
+					InitContainers:     []v1.Container{initContainer},
+				},
+			},
+		},
+	}
+
+	context, err := getContext()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	options, err := k8s.GetOpts(context, ns)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	_, err = options.CreateJob(job)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println("ttl-", kind, "-job created successfully")
 }
